9efd8b72d4e88fcb520d95b6a2168c9d3af7ad0a
Bastian Venthur <venthur@debian.org>
2010-08-17 18:19:24 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-popcon',
+        version='1.0',
+        description="Python inteface to Debian's popcon database",
+        author='Bastian Venthur',
+        author_email='venthur@debian.org',
+        py_modules=['popcon'],
+        package_dir={"": "src"},
+        )
+
