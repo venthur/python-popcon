db96c5e8f2a0ad96600e275df260995fcf656811
Bastian Venthur <venthur@debian.org>
2016-09-28 13:54:46 +0200

    Added version string

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
+import sys
 from distutils.core import setup
 
-setup(name='python-popcon',
-        version='1.3',
-        description="Python inteface to Debian's popcon database",
-        author='Bastian Venthur',
-        author_email='venthur@debian.org',
-        url='http://github.com/venthur/python-popcon',
-        py_modules=['popcon'],
-        package_dir={"": "src"},
-        )
+sys.path.insert(0, 'src')
 
+import popcon
+
+
+setup(name='python-popcon',
+      version=popcon.__version__,
+      description="Python inteface to Debian's popcon database",
+      author='Bastian Venthur',
+      author_email='venthur@debian.org',
+      url='http://github.com/venthur/python-popcon',
+      py_modules=['popcon'],
+      package_dir={"": "src"},
+)
