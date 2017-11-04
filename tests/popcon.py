d86ed31310f8d0e2db18f96dd09300cefdc281ff
Bastian Venthur <venthur@debian.org>
2017-11-04 15:36:35 +0100

    Added some dummy tests

--- tests/popcon.py
+++ tests/popcon.py
@@ -11,6 +11,18 @@ class TestPopcon(unittest.TestCase):
         except NameError:
             self.fail()
 
+    def test_package(self):
+        popcon.package('reportbug')
+
+    def test_package_raw(self):
+        popcon.package_raw('reportbug')
+
+    def test_source_package(self):
+        popcon.source_package('reportbug')
+
+    def test_source_package_raw(self):
+        popcon.source_package_raw('reportbug')
+
 
 if __name__ == '__main__':
     unittest.main()
