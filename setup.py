9a15ab7fa40a04604f75591abe6fe70f611718b3
Bastian Venthur <venthur@debian.org>
2017-11-04 15:25:13 +0100

    fixed package name in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import setup
 exec(open('./popcon/version.py').read())
 
 
-setup(name='python-popcon',
+setup(
+    name='python-popcon',
     version=__version__,
     description="Python inteface to Debian's popcon database",
+    keywords='debian, popcon, popularity contest',
     author='Bastian Venthur',
     author_email='venthur@debian.org',
     url='http://github.com/venthur/python-popcon',
-    py_modules=['popcon'],
-    package_dir={"": "src"},
+    license='GPL2',
+    packages=['popcon'],
     install_requires=['xdg'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, <4',
 )
