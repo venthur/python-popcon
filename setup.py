bf8870c3257d2e63ea3d1f62d1899c0e8235e539
Bastian Venthur <venthur@debian.org>
2017-11-04 15:30:16 +0100

    Added long description

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 exec(open('./popcon/version.py').read())
 
-
 setup(
     name='python-popcon',
     version=__version__,
-    description="Python inteface to Debian's popcon database",
+    description="Python interface to Debian's popularity contest database",
+    long_description="This packages allows to query Debian's popularity contest database.",
     keywords='debian, popcon, popularity contest',
     author='Bastian Venthur',
     author_email='venthur@debian.org',
